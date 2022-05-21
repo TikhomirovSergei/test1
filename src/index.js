0ca246dd88ed028994871bed4403dab6a1b47781
Sergey Tihomirov <truebelievervalar@gmail.com>
2022-05-21 20:40:45 +0300

    add page

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { persistor, store } from './store/Index';
 import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
+  <HashRouter basename={process.env.PUBLIC_URL}>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
         <App />
-      </BrowserRouter>
-    </PersistGate>
-  </Provider>
+      </PersistGate>
+    </Provider>
+  </HashRouter>
 );
 
